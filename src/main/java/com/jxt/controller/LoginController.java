37c2cb5ea80cb57631daf30babf369d58f1ce498
juxiaotong <861045346@qq.com>
2020-08-13 21:17:23 +0800

    登录使用ajax

--- src/main/java/com/jxt/controller/LoginController.java
+++ src/main/java/com/jxt/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import javax.servlet.http.HttpSession;
@@ -29,19 +30,37 @@ public class LoginController {
         if (user != null){
             if (password.equals(user.getPassWord())){
                 session.setAttribute("loginUser",username);
+                model.addAttribute("msg","ok");
                 return "redirect:/main.html";
             }else {
                 model.addAttribute("msg","密码错误");
                 return "index";
             }
         }else {
-            model.addAttribute("msg","密码为空");
+
+            model.addAttribute("msg","用户名不存在");
             return "index";
+        }
+    }
+
+    @RequestMapping("/ajax")
+    @ResponseBody
+    public String ajax(@RequestParam("username") String username){
+        User user = userService.getUserByName(username);
+        String msg = "";
+        if (username == null || "".equals(username)){
+            msg = "用户名不能为空";
+        } else {
+            if (user != null){
+                msg = "ok";
+            }else{
+                msg = "用户名不存在";
+            }
         }
 
 
-
-
+        return msg;
     }
+
 
 }
